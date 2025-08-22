7c31e88d7c796c962cb696589fea7ac2148cd103
Jani Eväkallio <jani.evakallio@gmail.com>
2025-08-22 18:29:27 +0100

    chore: explicit exports to make symlink resolution work in marker monorepo. This commit can be dropped if we publish the package to npm with prebuilt dist

--- src/y-prosemirror.js
+++ src/y-prosemirror.js
@@ -1,11 +1,47 @@
-export * from './plugins/cursor-plugin.js'
-export { ySyncPlugin, isVisible, getRelativeSelection, ProsemirrorBinding, updateYFragment } from './plugins/sync-plugin.js'
-export * from './plugins/undo-plugin.js'
-export * from './plugins/keys.js'
 export {
-  absolutePositionToRelativePosition, relativePositionToAbsolutePosition, setMeta,
-  prosemirrorJSONToYDoc, yDocToProsemirrorJSON, yDocToProsemirror, prosemirrorToYDoc,
-  prosemirrorJSONToYXmlFragment, yXmlFragmentToProsemirrorJSON, yXmlFragmentToProsemirror,
-  prosemirrorToYXmlFragment, yXmlFragmentToProseMirrorRootNode, yXmlFragmentToProseMirrorFragment,
-  initProseMirrorDoc
-} from './lib.js'
+  defaultAwarenessStateFilter,
+  defaultCursorBuilder,
+  defaultSelectionBuilder,
+  createDecorations,
+  yCursorPlugin,
+} from "./plugins/cursor-plugin.js";
+
+export {
+  ySyncPlugin,
+  isVisible,
+  getRelativeSelection,
+  ProsemirrorBinding,
+  updateYFragment,
+} from "./plugins/sync-plugin.js";
+
+export {
+  undo,
+  redo,
+  undoCommand,
+  redoCommand,
+  defaultProtectedNodes,
+  defaultDeleteFilter,
+  yUndoPlugin,
+} from "./plugins/undo-plugin.js";
+
+export {
+  ySyncPluginKey,
+  yUndoPluginKey,
+  yCursorPluginKey,
+} from "./plugins/keys.js";
+export {
+  absolutePositionToRelativePosition,
+  relativePositionToAbsolutePosition,
+  setMeta,
+  prosemirrorJSONToYDoc,
+  yDocToProsemirrorJSON,
+  yDocToProsemirror,
+  prosemirrorToYDoc,
+  prosemirrorJSONToYXmlFragment,
+  yXmlFragmentToProsemirrorJSON,
+  yXmlFragmentToProsemirror,
+  prosemirrorToYXmlFragment,
+  yXmlFragmentToProseMirrorRootNode,
+  yXmlFragmentToProseMirrorFragment,
+  initProseMirrorDoc,
+} from "./lib.js";
